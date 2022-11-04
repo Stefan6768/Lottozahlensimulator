17c7ed7453963175b66630a4e4230ebcec4e23bd
Stefan <103416259+Stefan6768@users.noreply.github.com>
2022-11-04 12:14:37 +0100

    Initial Commit

---
+++ Lottozahlensimulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottozahlensimulator
+//
+//  Created by Stefan Schreiber on 04.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
